8ed424b9e181c94f30ba0f47e2a49b188706526e
Zijing Wu <wuzijing@seas.upenn.edu>
2023-02-19 09:24:52 -0500

    V0.1

--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -1,10 +1,15 @@
 from typing import List
+import math
 
 from torch.optim.lr_scheduler import _LRScheduler
 
 
 class CustomLRScheduler(_LRScheduler):
-    def __init__(self, optimizer, last_epoch=-1):
+    """
+    Create a new custom learning rate scheduler that inherits from LR scheduler
+    """
+
+    def __init__(self, optimizer, step_size, gamma, last_epoch=-1):
         """
         Create a new scheduler.
 
@@ -12,13 +17,18 @@ class CustomLRScheduler(_LRScheduler):
         if you need to add new parameters.
 
         """
-        # ... Your Code Here ...
+        self.step_size = step_size
+        self.gamma = gamma
         super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
 
     def get_lr(self) -> List[float]:
+        """
+        Get lr scheduler, output a list
+        """
         # Note to students: You CANNOT change the arguments or return type of
         # this function (because it is called internally by Torch)
 
-        # ... Your Code Here ...
-        # Here's our dumb baseline implementation:
-        return [i for i in self.base_lrs]
+        if self.last_epoch % self.step_size == 0 and self.last_epoch > 0:
+            return [base_lr * self.gamma for base_lr in self.base_lrs]
+        else:
+            return [base_lr for base_lr in self.base_lrs]
