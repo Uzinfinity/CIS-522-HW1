605cf31c6d2cfeadce745a684f744330d2b91af3
Zijing Wu <wuzijing@seas.upenn.edu>
2023-03-08 11:07:05 -0500

    v0.1

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -1,9 +1,30 @@
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 
 class Model(torch.nn.Module):
+    """
+    fast CNN
+    """
+
     def __init__(self, num_channels: int, num_classes: int) -> None:
-        ...
+        super(Model, self).__init__()
+
+        self.conv1 = nn.Conv2d(num_channels, 32, kernel_size=6, stride=2, padding=1)
+        self.relu1 = nn.ReLU()
+        self.pool1 = nn.MaxPool2d(kernel_size=3, stride=2)
+        self.fc = nn.Linear(32 * 7 * 7, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        ...
+        """
+        forward
+        """
+        x = self.conv1(x)
+        x = self.relu1(x)
+        x = self.pool1(x)
+
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
